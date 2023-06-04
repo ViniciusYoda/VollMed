ead4a156c830428790a126047572593d98c121b7
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-04 19:07:33 -0300

    mysql

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,8 +2,13 @@ package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +17,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("medicos")
 public class MedicoController {
 
+   @Autowired
+   private MedicoRepository repository;
+
    @PostMapping
-   public void cadastrar(@RequestBody DadosCadastroMedicos dados){
-      System.out.println(dados);
+   @Transactional
+   public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
+      repository.save(new Medico(dados));
    }
 }
