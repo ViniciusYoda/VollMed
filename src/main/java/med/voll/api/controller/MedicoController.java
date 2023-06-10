de15ebb91daa753239604306d2872331b7203384
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-10 13:38:33 -0300

    get

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 
 @RestController
 @RequestMapping("medicos")
@@ -24,5 +31,10 @@ public class MedicoController {
    @Transactional
    public void cadastrar(@RequestBody @Valid DadosCadastroMedicos dados){
       repository.save(new Medico(dados));
+   }
+
+   @GetMapping
+   public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort ={"nome"}) Pageable paginacao){
+      return repository.findAll(paginacao).map(DadosListagemMedico::new);
    }
 }
